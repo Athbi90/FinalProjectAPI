902ce2b687f19ca0da67eb47e0cd2d9c2ba286a8
halanzi <homoudnan@gmail.com>
2021-04-28 07:15:45 +0300

    deleting images in media and general clean-up

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   JWT_SECRET: "asupersecretkey",
-  JWT_EXPIRATION_MS: 90000000, // the token will expire 15 minutes from when it's generated
+  JWT_EXPIRATION_MS: 90000000, // the token will expire 25 hours from when it's generated
 };
